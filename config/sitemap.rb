7286d3b37199ee1066cfabd92de3b115eb22b2fc
Michael Nacos <m.nacos@gmail.com>
2017-08-10 01:19:07 +0100

    add SELF_URL for sitemap.rb and others

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,8 @@
 require 'rubygems'
 require 'sitemap_generator'
 
-SitemapGenerator::Sitemap.default_host = 'https://www.brookgreensupply.com'
+SitemapGenerator::Sitemap.default_host = ENV['SELF_URL']
+SitemapGenerator::Sitemap.default_host ||= 'https://www.brookgreensupply.com'
 SitemapGenerator::Sitemap.include_root = false
 SitemapGenerator::Sitemap.create do
   add '/', changefreq: 'weekly', priority: 1.0
