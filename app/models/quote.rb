2b961b8c8eb8eecb2fe4e5943f3439e7948e8745
Michael Nacos <m.nacos@gmail.com>
2017-08-25 20:28:53 +0100

    change formating of CMA results

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,26 @@
 class Quote < ApplicationRecord
   include ActiveModel::ForbiddenAttributesProtection
   enum usage_or_cost_period: [:year, :six_months, :quarter, :month]
+
+  def annual_usage
+    multiply_by = case usage_or_cost_period
+      when 'year' then 1
+      when 'six_months' then jj
+      when 'quarter' then 4
+      when 'month' then 12
+      else 1
+    end
+    Rails.logger.warn usage
+    annual = if cost
+      products = JSON.parse presented_products
+      multiply_by * (cost / products.first['rate1'].to_f) / 100.0
+    else
+      if usage
+        multiply_by * usage
+      else
+        0
+      end
+    end
+    annual.round
+  end
 end
