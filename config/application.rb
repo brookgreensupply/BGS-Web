80e924f5ef1cf5148007c0c5a67cf2b820693b5f
Michael Nacos <m.nacos@gmail.com>
2017-09-01 16:28:20 +0100

    set i18n.locale as well as default_locale

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module BrookGreen
     end
 
     config.i18n.default_locale = :'en-GB'
+    config.i18n.locale = :'en-GB'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
