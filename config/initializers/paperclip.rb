7d264135c47725a8b4707c68f9665d7c589486cf
Michael Nacos <m.nacos@gmail.com>
2017-09-11 16:20:46 +0100

    add aws-sdk require

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,3 @@
+require 'aws-sdk'
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
